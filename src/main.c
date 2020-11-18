7356d2e2febd7ee322c80b1a1b79700a91523599
Andrés Muñoz <andres.munoz.b@usach.cl>
2020-11-18 19:25:21 -0300

    CHG: Se agrega la escritura del archivo y se parametriza los niveles y hebras
    Signed-off-by: Andrés Muñoz <andres.munoz.b@usach.cl>

--- src/main.c
+++ src/main.c
@@ -4,17 +4,15 @@
 int main()
 {
 
-    char *name_file = "../data/64floats.raw";
-    unsigned long size = 64l;
+    char *name_file = "../data/65536floats.raw";
+    char *output_name_file = "65536floatssorted.raw";
+    unsigned long size = 65536l;
+    int threads = 4;
+    int levels = 2;
 
-    float *numbers = (float *)aligned_alloc(16, sizeof(float) * size);
-    
-    read_file(name_file, numbers, size); // LEER ARCHIVO
-
-    for (int i =0;i<1000;i++){
-        printf("i: %d\n", i);
-        omp_sort(numbers, size, 2, 2);
-    }
-
-    free(numbers);
+    float *numbers = (float *)aligned_alloc(16, sizeof(float) * size); // SE SOLICITA MEMORIA PARA LOS NUMEROS A ORDENAR
+    read_file(name_file, numbers, size);         // LEER ARCHIVO
+    omp_sort(numbers, size, levels, threads);    // SORT THE NUMBERS
+    write_file(output_name_file, numbers, size); //ESCRIBIR LOS DATOS ORDENADOS
+    free(numbers); // SE LIBERA LA MEMORIA UTILIZADA PARA LOS NUMEROS
 }
