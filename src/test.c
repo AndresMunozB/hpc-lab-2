2c819971f2942fa3bb8c0eb9c1da1bd1e9bf1cbe
Andrés Muñoz <andres.munoz.b@usach.cl>
2020-11-22 14:51:08 -0300

    aasdf
    Signed-off-by: Andrés Muñoz <andres.munoz.b@usach.cl>

--- src/test.c
+++ src/test.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
     if (get_opt(argc, argv, &name_file, &output_name_file, &size, &dValue, &levels, &threads) == 0)
         return 0;
 
-    int iterations = 1;
+    int iterations = 31;
     float *numbers = (float *)aligned_alloc(16, sizeof(float) * size); // SE SOLICITA MEMORIA PARA LOS NUMEROS A ORDENAR
     for (int z = 2; z < 20; z++)
     {
