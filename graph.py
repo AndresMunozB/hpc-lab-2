1e307a1c801ab2c64b22fb820b0573a90d9cd4ee
Andrés Muñoz <andres.munoz.b@usach.cl>
2020-11-22 13:06:33 -0300

    asldkfj
    Signed-off-by: Andrés Muñoz <andres.munoz.b@usach.cl>

--- graph.py
+++ graph.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+from numpy import mean
+
 def read_file(name_file):
 	data_frame = pd.read_csv(name_file)
 	return data_frame
@@ -8,7 +11,34 @@ def graph_data(x,y):
     plt.plot(x,y)
     plt.show()
 
-data = []
+def graph_boxplot(data, filename, title, evaluation_criteria):
+    ax = sns.boxplot(x='instance', y= evaluation_criteria, data= data, palette = "BuGn_r")
+    ax = sns.swarmplot(x="instance", y= evaluation_criteria, data= data, color=".25")
+    ax.set_title(title)
+    ax.figure.savefig(filename,format='jpeg')
+    plt.clf()
+
+def graph_barplot(data,x,y,filename):
+    ax = sns.barplot(x=x, y=y, data=data, estimator=mean)
+    ax.figure.savefig(filename+".jpeg",format='jpeg')
+def append_data(data,threads,filename):
+    file = open(filename, 'a+')
+    for i in data:
+        file.write(""+str(threads)+","+str(i)+"\n")
+    file.close()
+    
+
+def unify_data(name_list,outputfile):
+    file = open(outputfile, 'a+')
+    file.write("threads,time\n")
+    file.close()
+    for name in name_list:
+        data = read_file(name[0])
+        append_data(data['time'],name[1],outputfile)
+
+
+    
+'''data = []
 threads = ["1h", "4h", "10h", "15h"]
 for thread in threads:
     tmp = read_file("65536_2l_" + thread+ ".csv")
@@ -22,5 +52,17 @@ plt.title("Los 11 mejores resultados")
 plt.ylabel("Cantidad de trabajadores normalizado")
 plt.xlabel("Iteraciones")
 plt.legend(loc='best')
-plt.show()
+plt.show()'''
+
+def get_name_list(base, levels, threads):
+    names = []
+    for i in threads:
+        names.append(base+"_"+str(levels)+"l"+"_"+str(i)+"h.csv")
+    return names
+ 
+threads = [1,5,10,15,20,25,30,35]
+name_list = get_name_list("16000000",2,threads)
+unify_data(name_list,"2_levels.csv")
+data = read_file("2_levels.csv")
+graph_barplot(data,"threads", "time", "barplot2")
 
