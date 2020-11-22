948d771e11be7cda8ca863c94ed3ebbcfd8eaab6
Cristian <cristian.espinoza.s@usach.cl>
2020-11-22 17:16:08 -0300

    ADD: grafic

--- graph.py
+++ graph.py
@@ -21,6 +21,7 @@ def graph_boxplot(data, filename, title, evaluation_criteria):
 def graph_barplot(data,x,y,filename):
     ax = sns.barplot(x=x, y=y, data=data, estimator=mean)
     ax.figure.savefig(filename+".jpeg",format='jpeg')
+
 def append_data(data,threads,filename):
     file = open(filename, 'a+')
     for i in data:
@@ -36,34 +37,29 @@ def unify_data(name_list,outputfile):
         data = read_file(name[0])
         append_data(data['time'],name[1],outputfile)
 
-
-    
-'''data = []
-threads = ["1h", "4h", "10h", "15h"]
-for thread in threads:
-    tmp = read_file("65536_2l_" + thread+ ".csv")
-    data.append(tmp['time'])
-count = 0
-colors = ['black','red','gray','orange','gold','yellow','green','aqua','blue','indigo','pink']
-for instancia in data:
-    plt.scatter(list(range(1,len(instancia)+1)),instancia,c=colors[count],label="Iteración" + str(count+1),s=15)
-    count = count + 1
-plt.title("Los 11 mejores resultados")
-plt.ylabel("Cantidad de trabajadores normalizado")
-plt.xlabel("Iteraciones")
-plt.legend(loc='best')
-plt.show()'''
-
 def get_name_list(base, levels, threads):
     names = []
-    for i in threads:
-        names.append([base+"_"+str(levels)+"l"+"_"+str(i)+"h.csv",i])
+    for j in range(0,levels):
+        for i in threads:
+            names.append([base+"_"+str(j)+"l"+"_"+str(i)+"h.csv" , j , i])
     return names
- 
-threads = [1,5,10,15,20,25,30,35]
-name_list = get_name_list("16000000",2,threads)
-#unify_data(name_list,"2_levels.csv")
-data = read_file("2_levels.csv")
-graph_boxplot(data, "boxplot.jpeg", "title", "time")
-graph_barplot(data,"threads", "time", "barplot2")
 
+def mean(names,name_out):
+    file_out = open(name_out, 'w')
+    file_out.write("name,levels,threads,mean\n")
+    for name in names:
+        data = read_file(name[0])
+        mean = data['time'].mean()
+        file_out.write(name[0] + "," + str(name[1]) + "," + str(name[2]) + "," + str(mean) + "\n")
+    file_out.close()
+
+def graph(name):
+    data = read_file(name)
+    print(data)
+
+threads = [1,5,9,13,17,21,25,29,33,37,41]
+levels = 20
+file_out = "promedios.csv"
+name_list = get_name_list("64",levels,threads)
+mean(name_list,file_out)
+graph(file_out)
