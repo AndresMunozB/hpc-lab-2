1628fe5687f40a145ac589a79325d2e9cbae10d6
Andrés Muñoz <andres.munoz.b@usach.cl>
2020-11-22 21:22:18 -0300

    CHG: Se arregla lo del debug
    Signed-off-by: Andrés Muñoz <andres.munoz.b@usach.cl>

--- src/main.c
+++ src/main.c
@@ -15,13 +15,13 @@ int main(int argc, char *argv[])
 
     float *numbers = (float *)aligned_alloc(16, sizeof(float) * size); // SE SOLICITA MEMORIA PARA LOS NUMEROS A ORDENAR
     read_file(name_file, numbers, size);                               // LEER ARCHIVO
-    clock_t t = clock();                                               // SE COMIENZA A MEDIR EL TIEMPO
-    omp_sort(numbers, size, levels, threads);                          // SORT THE NUMBERS
-    double time_taken = ((double)(clock() - t)) / CLOCKS_PER_SEC;      // SE TERMINA DE MEDIR EL TIEMPO
-    write_file(output_name_file, numbers, size);                       //ESCRIBIR LOS DATOS ORDENADOS
+    //clock_t t = clock();                                               // SE COMIENZA A MEDIR EL TIEMPO
+    omp_sort(numbers, size, levels, threads); // SORT THE NUMBERS
+    //double time_taken = ((double)(clock() - t)) / CLOCKS_PER_SEC;      // SE TERMINA DE MEDIR EL TIEMPO
+    write_file(output_name_file, numbers, size); //ESCRIBIR LOS DATOS ORDENADOS
     if (dValue)
-        printf("sort() took %f seconds to execute \n", time_taken);
-    print_float_array(numbers,size);
+        //printf("sort() took %f seconds to execute \n", time_taken); // SE IMPRIME EL TIEMPO TRANSCURRIDO
+        print_float_array(numbers, size);
     free(numbers); // SE LIBERA LA MEMORIA UTILIZADA PARA LOS NUMEROS
     return 0;
 }
