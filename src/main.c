7b51ecba6b471c1fbb7f8d5bf335530ce7f6b4b9
Andrés Muñoz <andres.munoz.b@usach.cl>
2020-11-22 22:28:05 -0300

    CHG: Limpiando codigo y agregando comentarios
    Signed-off-by: Andrés Muñoz <andres.munoz.b@usach.cl>

--- src/main.c
+++ src/main.c
@@ -10,17 +10,16 @@ int main(int argc, char *argv[])
     int threads;
     int levels;
     int dValue;
+
+    //SE OBTIENEN Y VALIDAN LOS DATOS DE ENTRADA
     if (get_opt(argc, argv, &name_file, &output_name_file, &size, &dValue, &levels, &threads) == 0)
-        return 0;
+        return 0; // TERMINAR EJECUCIÓN POR ERROR EN LOS PARÁMETROS
 
     float *numbers = (float *)aligned_alloc(16, sizeof(float) * size); // SE SOLICITA MEMORIA PARA LOS NUMEROS A ORDENAR
     read_file(name_file, numbers, size);                               // LEER ARCHIVO
-    //clock_t t = clock();                                               // SE COMIENZA A MEDIR EL TIEMPO
-    omp_sort(numbers, size, levels, threads); // SORT THE NUMBERS
-    //double time_taken = ((double)(clock() - t)) / CLOCKS_PER_SEC;      // SE TERMINA DE MEDIR EL TIEMPO
-    write_file(output_name_file, numbers, size); //ESCRIBIR LOS DATOS ORDENADOS
+    omp_sort(numbers, size, levels, threads);                          // ORDENAMIENTO CON OPENMP
+    write_file(output_name_file, numbers, size);                       //ESCRIBIR LOS DATOS ORDENADOS
     if (dValue)
-        //printf("sort() took %f seconds to execute \n", time_taken); // SE IMPRIME EL TIEMPO TRANSCURRIDO
         print_float_array(numbers, size);
     free(numbers); // SE LIBERA LA MEMORIA UTILIZADA PARA LOS NUMEROS
     return 0;
